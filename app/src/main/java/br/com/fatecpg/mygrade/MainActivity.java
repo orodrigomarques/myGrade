446c82f689def6d78b5cbfd861e0dbd7b64fc85c
rmark3z <r.marqu3s@gmail.com>
2016-11-08 12:39:12 -0200

    Update Main

--- app/src/main/java/br/com/fatecpg/mygrade/MainActivity.java
+++ app/src/main/java/br/com/fatecpg/mygrade/MainActivity.java
@@ -1,19 +1,22 @@
-package br.com.fatecpg.mygrade;
+package com.app.mygrades.mygrades;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import java.io.File;
 import java.util.ArrayList;
 
-
-
 public class MainActivity extends AppCompatActivity {
-
     File dirFiles[]; //Pasta de aqrquivos
     ArrayList<String> arquivos = new ArrayList<>();
 
@@ -21,8 +24,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                    Intent i = new Intent(getApplicationContext(), CadastrarActivity.class);
+                    startActivity(i);
+                }
+
+        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onResume() {
         super.onResume();
@@ -49,13 +87,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void adicionarDisciplina(View view){
-        Intent i = new Intent(getApplicationContext(), CadastrarActivity.class);
-        startActivity(i);
-    }
+
 
 }
-
-
-
 
